263caf97e2cbfb741ae4deb65e326ee1c75584d2
iluobei <iluobei@outlook.com>
2026-05-14 14:33:32 +0800

    v0.7.0

--- miaomiaowu/src/hooks/use-version-check.ts
+++ miaomiaowu/src/hooks/use-version-check.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 
-const CURRENT_VERSION = '0.6.9'
+const CURRENT_VERSION = '0.7.0'
 const GITHUB_API_URL = 'https://api.github.com/repos/iluobei/miaomiaowu/releases/latest'
 
 interface GitHubRelease {
